8035b591e24e7433479fc9724564d7516a04d94b
Ryuto77 <shojilkuriankk@gmail.com>
2024-03-03 19:14:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,12 +7,12 @@ API_HASH = os.environ.get("API_HASH", "ffd294313d574aa52d04813ee7dedb53")
 #BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
 TOKEN_ONE = os.environ.get("TOKEN_ONE", "6750339147:AAFoCm6xqXIofFnmdg7cujyUyfMMuMuEmPo")
 
-CHANNEL = os.environ.get("CHANNEL", "") # username without '@'
+CHANNEL = os.environ.get("CHANNEL", "AniDiscuz") # username without '@'
 BOT_USERNAME = os.environ.get("BOT_USERNAME","renext_bot") # username without '@'
 SUPPORT_GROUP = os.environ.get("SUPPORT_GROUP","AniDiscuz") # username without '@'
 UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL","Manga_Valley") # username without '@'
 OWNER_USERNAME = os.environ.get("OWNER_USERNAME","Ryuto77")
-STRING = os.environ.get("STRING", "")
+#STRING = os.environ.get("STRING", "")
 
 DB_NAME = os.environ.get("DB_NAME","renext_bot")     
 DB_URL = os.environ.get("DB_URL","mongodb+srv://renext_bot:renext_bot@cluster7.v4tgq6h.mongodb.net/?retryWrites=true&w=majority&appName=Cluster7")
