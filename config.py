6b0961389c98d9415582868e37eaf0d18cb136b1
Ryuto77 <shojilkuriankk@gmail.com>
2024-03-03 19:01:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,10 +8,10 @@ API_HASH = os.environ.get("API_HASH", "ffd294313d574aa52d04813ee7dedb53")
 TOKEN_ONE = os.environ.get("TOKEN_ONE", "6750339147:AAFoCm6xqXIofFnmdg7cujyUyfMMuMuEmPo")
 
 CHANNEL = os.environ.get("CHANNEL", "") # username without '@'
-BOT_USERNAME = os.environ.get("BOT_USERNAME","") # username without '@'
-SUPPORT_GROUP = os.environ.get("SUPPORT_GROUP","") # username without '@'
-UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL","") # username without '@'
-OWNER_USERNAME = os.environ.get("OWNER_USERNAME","")
+BOT_USERNAME = os.environ.get("BOT_USERNAME","renext_bot") # username without '@'
+SUPPORT_GROUP = os.environ.get("SUPPORT_GROUP","AniDiscuz") # username without '@'
+UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL","Manga_Valley") # username without '@'
+OWNER_USERNAME = os.environ.get("OWNER_USERNAME","Ryuto77")
 STRING = os.environ.get("STRING", "")
 
 DB_NAME = os.environ.get("DB_NAME","renext_bot")     
@@ -19,6 +19,6 @@ DB_URL = os.environ.get("DB_URL","mongodb+srv://renext_bot:renext_bot@cluster0.y
 
 FLOOD = int(os.environ.get("FLOOD", "90"))
 LAZY_PIC = os.environ.get("LAZY_PIC", "")
-ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '1989750989').split()]
+ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '1624191616').split()]
 PORT = os.environ.get('PORT', '8080')
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001230390281"))
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001504426044"))
